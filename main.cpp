3784ed5ea2da73f487b4f9b70376ce08d8b3a928
Donald Davies <40407408+Dcdavies243@users.noreply.github.com>
2018-09-12 15:12:34 +0100

    Complete project

--- main.cpp
+++ main.cpp
@@ -39,6 +39,16 @@ int main()
 	titleText.setStyle(sf::Text::Bold | sf::Text::Italic);
 	titleText.setPosition(gameWindow.getSize().x / 2 - titleText.getLocalBounds().width / 2, 30);
 
+
+	//Create prompt
+	sf::Text promptText;
+	promptText.setFont(gameFont);
+	promptText.setString("Click to start the game!");
+	promptText.setCharacterSize(75);
+	promptText.setFillColor(sf::Color::Red);
+	promptText.setStyle(sf::Text::Bold | sf::Text::Italic);
+	promptText.setPosition(gameWindow.getSize().x / 2 - promptText.getLocalBounds().width / 2, 200);
+
 	// Create Author Text
 	sf::Text authorText;
 	authorText.setFont(gameFont);
@@ -59,10 +69,45 @@ int main()
 	scoreText.setStyle(sf::Text::Bold | sf::Text::Italic);
 	scoreText.setPosition(75, 40);
 
+	// Create Timer Text
+	sf::Text timerText;
+	timerText.setFont(gameFont);
+	timerText.setCharacterSize(55);
+	timerText.setFillColor(sf::Color::Red);
+	timerText.setStyle(sf::Text::Bold | sf::Text::Italic);
+	timerText.setPosition(1500, 40);
+
+	// Timer Functionality
+	sf::Time timeLimit = sf::seconds(20.0f);
+	sf::Time timeRemaining = timeLimit;
+	sf::Clock gameClock;
+
+
+	//Click sound effect
+	sf::SoundBuffer clickBuffer;
+	clickBuffer.loadFromFile("audio/buttonclick.ogg");
+	sf::Sound clickSound;
+	clickSound.setBuffer(clickBuffer);
+
+	//Gameover sound effect
+	sf::SoundBuffer gameoverBuffer;
+	gameoverBuffer.loadFromFile("audio/gameover.ogg");
+	sf::Sound gameoverSound;
+	gameoverSound.setBuffer(gameoverBuffer);
+
+	//startgame sound effect
+	sf::SoundBuffer startgameBuffer;
+	startgameBuffer.loadFromFile("audio/gameover.ogg");
+	sf::Sound startgameSound;
+	startgameSound.setBuffer(startgameBuffer);
+
+	bool playing = false;
+
 	//Create Music
 	sf::Music gameMusic;
 	gameMusic.openFromFile("audio/music.ogg");
 	//gameMusic.play();
+
 
 	//--------------------------------------------
 	// Game Loop
@@ -75,17 +120,68 @@ int main()
 		{
 			//TODO: Process events
 
+			//Check if the event is the Mouse button pressed event
+			if (gameEvent.type == sf::Event::MouseButtonPressed)
+			{
+				if (buttonSprite.getGlobalBounds().contains(gameEvent.mouseButton.x, gameEvent.mouseButton.y))
+				{
+					if (playing == true)
+					{
+						//Button was clicks
+						score = score + 1;
+					}
+					else
+					{
+						playing = true;
+
+						// reset the game data
+						score = 0;
+						timeRemaining = timeLimit;
+						startgameSound.play();
+					}
+
+
+
+					//Button was clicks
+					score = score + 1;
+
+					
+					clickSound.play();
+				}
+
+				
+			}
+
 			//Check if the event is the closed event
 			if (gameEvent.type == sf::Event::Closed)
 			{
 				gameWindow.close();
 			}
 
+
 		}
 
 		//TODO: Update game state
-		score = score + 1;
+		sf::Time frameTime = gameClock.restart();
+		if (playing)
+		{
+			timeRemaining = timeRemaining - frameTime;
+
+			if (timeRemaining.asSeconds() <= 0.0f)
+			{
+				timeRemaining = sf::seconds(0.0f);
+				playing = false;
+				promptText.setString("Your final score was " + std::to_string(score) + "!\n Click the button to start a new game");
+				
+				gameoverSound.play();
+			}
+		}
+
+
+		//update our text displays based on data
+		timerText.setString("Timer: " + std::to_string((int)timeRemaining.asSeconds()));
 		scoreText.setString("Score: " + std::to_string(score));
+
 
 		//TODO: Draw graphics
 		//Clear the window to a single colour
@@ -96,6 +192,8 @@ int main()
 		gameWindow.draw(titleText);
 		gameWindow.draw(authorText);
 		gameWindow.draw(scoreText);
+		gameWindow.draw(timerText);
+		gameWindow.draw(promptText);
 
 		gameWindow.display();
 	}
